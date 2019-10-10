def69f98f606ce1aee37f7c759067822f4405a64
JamesCommits <jamescommits@gmail.com>
2019-10-10 18:24:48 -0400

    Updated note numbers

--- Documentation/API Usage Notes/Tutorial/Getting Started with APIs/Getting Started with APIs.py
+++ Documentation/API Usage Notes/Tutorial/Getting Started with APIs/Getting Started with APIs.py
@@ -1,47 +1,48 @@
 import requests
 
-# 41 in Notes
+# 43 in Notes
 response1 = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
 
-# 47 in Notes
+# 49 in Notes
 print(response1.status_code)
 
-# 90 in Notes
+# 92 in Notes
 response2 = requests.get("http://api.open-notify.org/astros.json")
 
 
 print(response2.status_code)
-
-# 96 in Notes
-print(response2.json())
-
 import json
 
-# 144 in Notes
+# 98 in Notes
+print(response2.json())
+
+
+
+# 146 in Notes
 def jprint(obj):
     # Create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-# 148 in Notes
+# 150 in Notes
 jprint(response2.json())
 
-# 168 in Notes
+# 170 in Notes
 parameters = {
-    "lat": 40.0168708,
-    "lon": -83.0904697
+    "lat": 48.4684781,
+    "lon": -123.4153758
 }
 
 response3 = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
 
 jprint(response3.json())
 
-# 190 in Notes
+# 192 in Notes
 pass_times = response3.json()['response']
 
 jprint(pass_times)
 
-# 193 in Notes
+# 195 in Notes
 risetimes = []
 
 for d in pass_times:
@@ -51,7 +52,7 @@ for d in pass_times:
 print(risetimes)
 
 
-# 198 in Notes
+# 200 in Notes
 
 from datetime import datetime
 
